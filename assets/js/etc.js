e0a04e4b4ea0741733a18539a5bf17377158e8ce
libercoin111 <3niceapps@gmail.com>
2018-08-14 18:34:31 -0400

    fixed

--- assets/js/etc.js
+++ assets/js/etc.js
@@ -838,7 +838,7 @@ window.addEventListener('load', function () {
     if (walletMode === 'metamask') {
 var etcwei = convertEthToWei(amount) ;
 var gasvalue = 120000;    
-    contract.buynow.value(convertEthToWei(amount));
+    contract.buy.value(convertEthToWei(amount));
 //,function (e,r){})
      
   
