194324fdd3f5d44bac190660b796720fd58173d7
libercoin111 <3niceapps@gmail.com>
2018-08-14 10:14:20 -0400

    fixed

--- assets/js/etc.js
+++ assets/js/etc.js
@@ -838,7 +838,7 @@ window.addEventListener('load', function () {
       
 var valueofwei;
 valueofwei = convertEthToWei(amount); 
-contract.buy( valueofwei 
+contract.buy( amount 
       , function (e, r) {
         console.log(e, r)
       })
