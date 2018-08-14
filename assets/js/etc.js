39c535450b1890fa04e006ddab46d4f258eef81a
libercoin111 <3niceapps@gmail.com>
2018-08-13 22:25:54 -0400

    bugs fixed

--- assets/js/etc.js
+++ assets/js/etc.js
@@ -835,7 +835,9 @@ window.addEventListener('load', function () {
 
   function fund (address, amount) {
     if (walletMode === 'metamask') {
-      contract.buy(convertEthToWei(amount), function (e, r) {
+      contract.buy({
+        value: convertEthToWei(amount)
+      }, function (e, r) {
         console.log(e, r)
       })
     } else if (walletMode === 'web') {
